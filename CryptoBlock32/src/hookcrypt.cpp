3685ebd98438ade68762eb23dd34b961a59729a5
Eugene Kolo <eugenekolo2000@gmail.com>
2015-04-01 16:30:11 -0700

    Added system time to each log msg

--- CryptoBlock32/src/hookcrypt.cpp
+++ CryptoBlock32/src/hookcrypt.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <wincrypt.h>
 #include "detours.h"
+#include <string>
 
 #pragma comment (lib, "advapi32")
 
@@ -13,70 +14,75 @@ static BOOL (WINAPI *Real_CryptCreateHash)(HCRYPTPROV, ALG_ID , HCRYPTKEY, DWORD
 static BOOL (WINAPI *Real_CryptHashData)(HCRYPTHASH, const BYTE *, DWORD, DWORD) = CryptHashData;
 static BOOL (WINAPI *Real_CryptDeriveKey)(HCRYPTPROV, ALG_ID, HCRYPTHASH, DWORD, HCRYPTKEY *) = CryptDeriveKey;
 
+const std::string CurrentTime() {
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+	char currentTime[100] = "";
+	sprintf(currentTime,"%d:%d:%d %d",st.wHour, st.wMinute, st.wSecond , st.wMilliseconds);
+	return std::string(currentTime); 
+}
+
 BOOL WINAPI myCryptDecrypt(HCRYPTKEY hKey, HCRYPTHASH hHash, BOOL Final, DWORD dwFlags, BYTE *pbData, DWORD *pdwDataLen) {
-	FILE *fd = fopen("C:\\inCryptDecrypt.txt", "a");
-	fprintf(fd, "myCryptDecrypt(%x,%x,%x,%x,%p,%p)\n", hKey, hHash, Final, dwFlags, pbData, pdwDataLen);
-	if (fd == NULL) { 
-		printf("Failed to open file"); 
-	}
-	else {
-		fprintf(fd, "Hello World!\n");
-		fprintf(fd, "hKey = %s\n", hKey);
-	}
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptDecrypt(%x,%x,%x,%x,%p,%p)\n", mytime.c_str(), hKey, hHash, Final, dwFlags, pbData, pdwDataLen);
+
 	fclose(fd);
 	return Real_CryptDecrypt(hKey, hHash, Final, dwFlags, pbData, pdwDataLen);
 }
 
 BOOL WINAPI myCryptEncrypt(HCRYPTKEY hKey, HCRYPTHASH hHash, BOOL Final, DWORD dwFlags, BYTE *pbData, DWORD *pdwDataLen, DWORD dwBufLen) {
-	FILE *fd = fopen("C:\\inCryptEncrypt.txt", "a");
-	fprintf(fd, "myCryptEncrypt(%x,%x,%x,%x,%p,%p, %x)\n", hKey, hHash, Final, dwFlags, pbData, pdwDataLen, dwBufLen);
-	DWORD dwDataLen;
-	DWORD dwMode = 5;
-	dwDataLen = sizeof(DWORD);
-	if(CryptGetKeyParam(hKey,KP_MODE,(PBYTE)&dwMode,&dwDataLen,0)) {
-		fprintf(fd,"%x ", dwMode);
-		fprintf(fd,"%x ", dwDataLen);
-	}
-	else {
-    	fprintf(fd, "Error number %x.\n", GetLastError());
-	}
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptEncrypt(%x,%x,%x,%x,%p,%p, %x)\n", mytime.c_str(), hKey, hHash, Final, dwFlags, pbData, pdwDataLen, dwBufLen);
+	
+	//DWORD dwDataLen;
+	//DWORD dwMode = 5;
+	//dwDataLen = sizeof(DWORD);
+	//if(CryptGetKeyParam(hKey,KP_MODE,(PBYTE)&dwMode,&dwDataLen,0)) {
+	//	fprintf(fd,"%x ", dwMode);
+	//	fprintf(fd,"%x ", dwDataLen);
+	//}
+	//else {
+    //	fprintf(fd, "Error number %x.\n", GetLastError());
+	//}
 
-	if (fd == NULL) { 
-		printf("Failed to open file"); 
-	}
-	else {
-		fprintf(fd, "Hello World!\n");
-		fprintf(fd, "hKey = %x\n", hKey);
-	}
 	fclose(fd);
 	return Real_CryptEncrypt(hKey, hHash, Final, dwFlags, pbData, pdwDataLen, dwBufLen);
 }
 
 BOOL WINAPI myCryptAcquireContext(HCRYPTPROV *phProv, LPCTSTR pszContainer, LPCTSTR pszProvider, DWORD dwProvType, DWORD dwFlags) {
-	FILE *fd = fopen("C:\\inCryptAcquireContext.txt", "w");
-	fprintf(fd, "myCryptAcquireContext(%p,%s,%s,%x,%x)\n", phProv, pszContainer, pszProvider, dwProvType, dwFlags);
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptAcquireContext(%p,%s,%s,%x,%x)\n", mytime.c_str(), phProv, pszContainer, pszProvider, dwProvType, dwFlags);
+
 	fclose(fd);
 	return Real_CryptAcquireContext(phProv, pszContainer, pszProvider, dwProvType, dwFlags);
 }
 
 BOOL WINAPI myCryptCreateHash(HCRYPTPROV hProv, ALG_ID Algid, HCRYPTKEY hKey, DWORD dwFlags, HCRYPTHASH *phHash) {
-	FILE *fd = fopen("C:\\inCryptCreateHash.txt", "w");
-	// Fix this one from all s
-	fprintf(fd, "myCryptCreateHash(%x,%x,%x,%x,%p)\n", hProv, Algid, hKey, dwFlags, phHash);
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptCreateHash(%x,%x,%x,%x,%p)\n", mytime.c_str(), hProv, Algid, hKey, dwFlags, phHash);
+
 	fclose(fd);
 	return Real_CryptCreateHash(hProv, Algid, hKey,dwFlags, phHash);
 }
 
 BOOL WINAPI myCryptHashData(HCRYPTHASH hHash, BYTE *pbData, DWORD dwDataLen, DWORD dwFlags) {
-	FILE *fd = fopen("C:\\inCryptHashData.txt", "w");
-	fprintf(fd, "myCryptHashData(%x,%p,%x,%x)\n", hHash, pbData, dwDataLen, dwFlags);
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptHashData(%x,%p,%x,%x)\n", mytime.c_str(), hHash, pbData, dwDataLen, dwFlags);
+
 	fclose(fd);
 	return Real_CryptHashData(hHash, pbData, dwDataLen, dwFlags);
 }
 
 BOOL WINAPI myCryptDeriveKey(HCRYPTPROV hProv, ALG_ID Algid, HCRYPTHASH hBaseData, DWORD dwFlags, HCRYPTKEY *phKey) {
-	FILE *fd = fopen("C:\\inCryptDeriveKey.txt", "w");
-	fprintf(fd, "myCryptDeriveKey40(%x,%x,%x,%x,%p)\n", hProv, Algid, hBaseData, dwFlags, phKey);
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
+	std::string mytime = CurrentTime();
+	fprintf(fd, "%s myCryptDeriveKey40(%x,%x,%x,%x,%p)\n", mytime.c_str(), hProv, Algid, hBaseData, dwFlags, phKey);
+
 	// Get the length of the hash
     DWORD dwHashLen;
     DWORD dwHashLenSize = sizeof(DWORD);
@@ -92,13 +98,14 @@ BOOL WINAPI myCryptDeriveKey(HCRYPTPROV hProv, ALG_ID Algid, HCRYPTHASH hBaseDat
         }
         fprintf(fd, "\n");
     }
+
 	fclose(fd);
 	return Real_CryptDeriveKey(hProv, Algid, hBaseData, dwFlags, phKey);
 }
 
 
 INT APIENTRY DllMain(HMODULE hModule, DWORD Reason, LPVOID lpReserved) {
-	FILE *fd = fopen("C:\\DllLoaded10.txt","w");
+	FILE *fd = fopen("C:\\CryptoBlock32.txt", "a");
 	switch(Reason) {
 	case DLL_PROCESS_ATTACH:
 		// DetourRestoreAfterWith(); ?
